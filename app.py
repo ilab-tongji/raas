5ad39f28fb98ad5d6c6bb7d0e2ab461dff6e309e
BelleZhao <15221373597@163.com>
2017-09-27 11:18:54 +0800

    问路路由

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 from __future__ import print_function
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory,render_template, request, session, redirect, url_for, jsonify
 from flask_cors import CORS
+from qa import storage
 import os
 
 
@@ -16,12 +17,19 @@ CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'hello world'
+    return render_template('homepage.html')
 
 
 @app.route('/getfile/<string:filename>', methods=['GET'])
 def get_file(filename):
     return send_from_directory('static', filename)
+
+@app.route('/qa/map',methods=['GET','POST'])
+def get_location():
+    location=request.form['location']
+    ans=storage.query('map',{'location':location})
+    return render_template('homepage.html',answer=ans)
+
 
 
 if __name__ == '__main__':
